c98f94c7af77f492c6eeffed37406cd1d5bf5cdb
bgaze <benjamin@bgaze.fr>
2018-03-06 11:37:56 +0400

    Migration base

--- src/Console/Crud/MigrateMakeCommand.php
+++ src/Console/Crud/MigrateMakeCommand.php
@@ -3,6 +3,9 @@
 namespace Bgaze\Crud\Console\Crud;
 
 use Illuminate\Database\Console\Migrations\MigrateMakeCommand as Base;
+use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Support\Composer;
+use Bgaze\Crud\Support\MigrateField;
 
 class MigrateMakeCommand extends Base {
 
@@ -22,5 +25,86 @@ class MigrateMakeCommand extends Base {
      * @var string
      */
     protected $description = 'Create a new CRUD migration file';
+
+    /**
+     *
+     * @var \Illuminate\Database\Eloquent\Collection 
+     */
+    protected $columns;
+
+    /**
+     *
+     * @var array 
+     */
+    protected $migration = [];
+
+    /**
+     * 
+     */
+    public function __construct(MigrationCreator $creator, Composer $composer) {
+        parent::__construct($creator, $composer);
+
+        $this->columns = collect(config('crud_dic.migrate.fields'))->map(function($v, $k) {
+            $tmp = new MigrateField($k, $v['signature'], $v['description'], $v['template']);
+            $tmp->setRules(isset($v['validation']) ? $v['validation'] : [], config('crud_dic.migrate.validation'));
+            return $tmp;
+        });
+    }
+
+    public function handle() {
+        //parent::handle();
+        $this->getColumns();
+    }
+
+    protected function getColumns() {
+        $reg = '/^(' . $this->columns->keys()->implode('|') . ')(\s.*)?$/';
+
+        $this->warn('Table columns');
+        $this->line('To see available column types, enter "list".');
+        $this->line('To see detailed syntax for a column, omit arguments and options.');
+
+        while (true) {
+            $question = $this->anticipate('Add a column ?', $this->columns->keys()->toArray(), 'no');
+
+            if ($question === 'no') {
+                break;
+            }
+
+            if ($question === 'list') {
+                $this->table(['Column name', 'Arguments', 'Options'], $this->columns->map(function ($v) {
+                            return $v->help(true);
+                        }));
+                continue;
+            }
+
+            try {
+                if (!preg_match($reg, $question, $m)) {
+                    throw new \Exception("Invalid input '$question'.");
+                }
+
+                $this->migration[] = $this->getColumn($m[1], isset($m[2]) ? trim($m[2]) : '');
+                var_dump($this->migration);
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+                continue;
+            }
+        }
+    }
+
+    protected function getColumn($field, $question) {
+        if (!$this->columns->has($field)) {
+            throw new \Exception("Undefined field '$field'.");
+        }
+
+        $column = $this->columns->get($field);
+
+        if (empty($question)) {
+            $this->info($column->getDescription());
+            $this->line($column->help());
+            return;
+        }
+
+        $this->migration[] = $column->input($question);
+    }
 
 }
--- src/Console/CrudCommand.php
+++ src/Console/CrudCommand.php
@@ -3,10 +3,6 @@
 namespace Bgaze\Crud\Console;
 
 use Illuminate\Console\Command;
-use Bgaze\Crud\Console\SignatureCommand;
-use Illuminate\Console\Parser;
-use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Input\InputDefinition;
 
 class CrudCommand extends Command {
 
@@ -30,43 +26,7 @@ class CrudCommand extends Command {
      * @return mixed
      */
     public function handle() {
-        $fields = config('crud_dic.migrate.fields');
-        $q = false;
-
-        while ($q === false) {
-            $q = $this->anticipate('Add a field (empty = quit)', array_keys($fields));
-
-            if (empty($q)) {
-                break;
-            }
-
-            $name = explode(' ', $q)[0];
-            if (!isset($fields[$name])) {
-                $this->error("Undefined field '$name'.");
-                $q = false;
-                continue;
-            }
-
-            $signature = $fields[$name]['signature'];
-            list($name, $arguments, $options) = Parser::parse($signature);
-
-            $definition = new InputDefinition();
-            foreach ($arguments as $argument) {
-                $this->getDefinition()->addArgument($argument);
-            }
-            foreach ($options as $option) {
-                $this->getDefinition()->addOption($option);
-            }
-
-            $input = new StringInput($q);
-            $input->bind($definition);
-
-
-            var_dump($name);
-            var_dump($input->getArguments());
-            var_dump($input->getOptions());
-            die();
-        }
+        
     }
 
 }
