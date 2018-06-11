50c989802e4c1d0f89a70956cd65f817d0ddf096
Bgaze <benjamin@cahri.com>
2018-06-11 22:20:17 +0400

    Field class

--- src/Core/Field.php
+++ src/Core/Field.php
@@ -2,13 +2,123 @@
 
 namespace Bgaze\Crud\Core;
 
+use Illuminate\Support\Str;
 use Bgaze\Crud\Support\SignedInput;
 
 /**
- * Description of Field
+ * A content entry of a CRUD (field or index).
  *
- * @author bgaze
+ * @author bgaze <benjamin@bgaze.fr>
  */
 class Field extends SignedInput {
-    //put your code here
+
+    /**
+     * The configuration of the field.
+     * 
+     * @var \stdClass 
+     */
+    public $config;
+
+    /**
+     * The unique name of the field.
+     * 
+     * @var string 
+     */
+    public $name;
+
+    /**
+     * The label to use for the field.
+     * 
+     * @var string 
+     */
+    public $label;
+
+    /**
+     * The class constructor.
+     * 
+     * @param string $type      The field type. 
+     * @param string $question  The user signed input
+     */
+    public function __construct($type, $question) {
+        // Get fields configuration.
+        $this->config = (object) config("crud-definitions.fields.{$type}");
+
+        // Instanciate field.
+        parent::__construct($type . ' ' . $this->config('signature'));
+
+        // Set & validate user input.
+        $this->ask($question);
+        $this->validate(config('crud-definitions.validation'));
+
+        // Set field name.
+        $this->setName();
+
+        // Set field label.
+        $this->setLabel();
+    }
+
+    /**
+     * Generate the label of the content.
+     * 
+     * @return string
+     */
+    protected function setLabel() {
+        $this->label = ucfirst(str_replace('_', ' ', Str::snake($this->name)));
+    }
+
+    /**
+     * Generate the unique name of the content.
+     * 
+     * @return string
+     */
+    protected function setName() {
+        if ($this->isIndex()) {
+            $this->name = 'index:' . implode('_', $this->input()->getArgument('columns'));
+        } else {
+            $this->name = $this->input()->getArgument('column');
+        }
+    }
+
+    /**
+     * Get content's configuration entry by key.
+     * 
+     * @param string $key The key of the entry
+     * @param mixed $default The default value of the entry
+     * @return mixed
+     */
+    public function config($key, $default = false) {
+        if (!isset($this->config->{$key})) {
+            return $default;
+        }
+
+        return $this->config->{$key};
+    }
+
+    /**
+     * Check if the field is an index.
+     * 
+     * @return boolean
+     */
+    public function isIndex() {
+        return ($this->config('type') === 'index');
+    }
+
+    /**
+     * The unique name of the field.
+     * 
+     * @return string
+     */
+    function name() {
+        return $this->name;
+    }
+
+    /**
+     * The label to use for the field.
+     * 
+     * @return string
+     */
+    function label() {
+        return $this->label;
+    }
+
 }
