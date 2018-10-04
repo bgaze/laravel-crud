195f9d4689152dd26d5d7e5fc8d38f5e9bd51934
Bgaze <benjamin@cahri.com>
2018-10-04 14:27:38 +0400

    Use mix url

--- src/Themes/Vue/Views/layout.blade.php
+++ src/Themes/Vue/Views/layout.blade.php
@@ -3,24 +3,24 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         @section('metas')
-            <meta charset="utf-8">
-            <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         @show
-        
+
         <title></title>
-                
+
         @yield('stylesheets')
     </head>
-    
+
     <body>
         @section('body')
         <div id="app">
             <router-view></router-view>
         </div>  
         @show
-        
+
         @section('javascripts')
-            <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset(mix('/js/app.js')) }}"></script>
         @show
     </body>
 </html>
